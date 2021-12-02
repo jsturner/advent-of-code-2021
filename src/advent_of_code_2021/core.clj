e0bb0cec2b848dc946308d43cfc595d030f96942
Josh Turner <josh.turner@tcgplayer.com>
2021-12-02 10:51:20 -0500

    Simplified day 1

--- src/advent_of_code_2021/core.clj
+++ src/advent_of_code_2021/core.clj
@@ -1,8 +1,6 @@
 (ns advent-of-code-2021.core
   (:gen-class))
 
-(def testdata [199 200 208 210 200 207 240 269 260 263])
-
 (defn input1 [path] (map #(Integer/parseInt %)
                          (-> (slurp path)
                              (clojure.string/split-lines))))
@@ -10,21 +8,18 @@
 (defn- sums [coll]
   (reduce + (take 3 coll)))
 
-(defn depth-counter [coll prev acc]
-  (if (empty? coll)
-    (count acc)
-    (recur (rest coll)
-           (first coll)
-           (if (nil? prev) acc (if (> (first coll) prev) (cons prev acc) acc)))))
-
-(defn depth-counter2 [coll prev acc]
-  (if (empty? coll)
-    (count acc)
-    (recur (rest coll)
-           (sums coll)
-           (if (nil? prev) acc (if (> (sums coll) prev) (cons prev acc) acc)))))
+(defn depth-counter
+  ([coll measurement-fn]
+   (depth-counter coll nil [] measurement-fn))
+  ([coll prev acc measurement-fn]
+   (if (empty? coll)
+     (count acc)
+     (recur (rest coll)
+            (measurement-fn coll)
+            (if (nil? prev) acc (if (> (measurement-fn coll) prev) (cons prev acc) acc))
+            measurement-fn))))
 
 (defn -main
   [& args]
-  (println "The answer to part 1 is" (depth-counter (input1 "resources/input1") nil []))
-  (println "The answer to part 1 is" (depth-counter2 (input1 "resources/input1") nil [])))
+  (println "The answer to part 1 is" (depth-counter (input1 "resources/input1") first))
+  (println "The answer to part 1 is" (depth-counter (input1 "resources/input1") sums)))

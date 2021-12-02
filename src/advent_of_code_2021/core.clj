3ac93b5042759e71bc239821ea3e840931357e0a
Josh Turner <josh.turner@tcgplayer.com>
2021-12-02 09:13:34 -0500

    Day 1 Part 2

--- src/advent_of_code_2021/core.clj
+++ src/advent_of_code_2021/core.clj
@@ -1,9 +1,14 @@
 (ns advent-of-code-2021.core
   (:gen-class))
 
+(def testdata [199 200 208 210 200 207 240 269 260 263])
+
 (defn input1 [path] (map #(Integer/parseInt %)
                          (-> (slurp path)
                              (clojure.string/split-lines))))
+
+(defn- sums [coll]
+  (reduce + (take 3 coll)))
 
 (defn depth-counter [coll prev acc]
   (if (empty? coll)
@@ -12,9 +17,14 @@
            (first coll)
            (if (nil? prev) acc (if (> (first coll) prev) (cons prev acc) acc)))))
 
+(defn depth-counter2 [coll prev acc]
+  (if (empty? coll)
+    (count acc)
+    (recur (rest coll)
+           (sums coll)
+           (if (nil? prev) acc (if (> (sums coll) prev) (cons prev acc) acc)))))
+
 (defn -main
   [& args]
-  (println "The answer is" (depth-counter (input1 "resources/input1") nil [])))
-
-  ;;(def input (input1 "resources/input1"))
-  ;;(depth-counter input nil [])
+  (println "The answer to part 1 is" (depth-counter (input1 "resources/input1") nil []))
+  (println "The answer to part 1 is" (depth-counter2 (input1 "resources/input1") nil [])))

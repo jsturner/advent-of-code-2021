22e486628dac9c25a59e3cddab67f2bf7f12a2a1
Josh Turner <josh.turner@tcgplayer.com>
2021-12-01 20:26:07 -0500

    Day 1 Part 1

--- src/advent_of_code_2021/core.clj
+++ src/advent_of_code_2021/core.clj
@@ -1,7 +1,20 @@
 (ns advent-of-code-2021.core
   (:gen-class))
 
+(defn input1 [path] (map #(Integer/parseInt %)
+                         (-> (slurp path)
+                             (clojure.string/split-lines))))
+
+(defn depth-counter [coll prev acc]
+  (if (empty? coll)
+    (count acc)
+    (recur (rest coll)
+           (first coll)
+           (if (nil? prev) acc (if (> (first coll) prev) (cons prev acc) acc)))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "The answer is" (depth-counter (input1 "resources/input1") nil [])))
+
+  ;;(def input (input1 "resources/input1"))
+  ;;(depth-counter input nil [])

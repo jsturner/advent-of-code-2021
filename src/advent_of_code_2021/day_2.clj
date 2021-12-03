21bf6fbda99f07f44a12f43833a104df6b4dcdcb
Josh Turner <josh.turner@tcgplayer.com>
2021-12-03 17:16:49 -0500

    Day 2 Part 2

--- src/advent_of_code_2021/day_2.clj
+++ src/advent_of_code_2021/day_2.clj
@@ -27,25 +27,11 @@
     (* (:x coordinates) (:y coordinates))
     (recur (rest coll)
            (case (:direction (first coll))
-             "forward" (assoc coordinates :x (+ (:distance (first coll)) (:x coordinates))
-                              :y (* (:aim coordinates) (:distance (first coll))))
+             "forward" (assoc coordinates
+                              :x (+ (:distance (first coll)) (:x coordinates))
+                              :y (+ (:y coordinates) (* (:aim coordinates) (:distance (first coll)))))
              "down" (update coordinates :aim + (:distance (first coll)))
              "up" (update coordinates :aim - (:distance (first coll)))))))
 
-(def inputs [{:direction "forward", :distance 5}
-             {:direction "down", :distance 5}
-             {:direction "forward", :distance 8}
-             {:direction "up", :distance 3}
-             {:direction "down", :distance 8}
-             {:direction "forward", :distance 2}])
-
-
-(distance-traveled (input "resources/input2") current-location)
-(distance-traveled2 (input "resources/input2") current-location)
-
-(comment
-  (def testlocation {:x 5, :y 0, :aim 5})
-  (def testinput {:direction "forward", :distance 8})
-
-  (assoc testlocation :y (* (:aim testlocation) (:distance testinput)) :x (:distance testinput))
-  )
+(println "Part 1: " (distance-traveled (input "resources/input2") current-location))
+(println "Part 2: " (distance-traveled2 (input "resources/input2") current-location))

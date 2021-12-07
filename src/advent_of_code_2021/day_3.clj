45e7ecab45f3cdb99bd520fc6bcc648f94d386f1
Josh Turner <josh.turner@tcgplayer.com>
2021-12-07 11:50:30 -0500

    Day 3 Part 1 cleanup

--- src/advent_of_code_2021/day_3.clj
+++ src/advent_of_code_2021/day_3.clj
@@ -1,31 +1,40 @@
 (ns advent-of-code-2021.day-3)
 
+;; https://cek.io/blog/2017/08/17/clojure-introduction-binary-bitwise/
+
 (defn input
   [path]
   (-> (slurp path)
       (clojure.string/split-lines)))
 
-(defn- on-setting [reading index]
+(defn- on-setting
+  "returns true if a bit is 1"
+  [reading index]
   (bit-test (Integer/parseInt reading 2) index))
 
-(defn- off-setting [reading index]
+(defn- off-setting
+  "returns true if a bit is 0"
+  [reading index]
   (not (bit-test (Integer/parseInt reading 2) index)))
 
-(defn power-readout [reading measurement]
+(defn power-readout 
+  "Updates a vector representing a running tally of how many
+   times each bit is 1 or 0 (on or off)"
+  [reading measurement]
   (loop [x (- (count reading) 1)
-         score (vec (repeat (count reading) 0))]
+         tally (vec (repeat (count reading) 0))]
     (if (< x 0)
-      score
+      tally
       (recur (- x 1)
              (if (measurement reading x)
-               (update score (- 11 x) inc)
-               score)))))
+               (update tally (- 11 x) inc)
+               tally)))))
 
-(defn bit-comparison [i a b]
-  (if (> (get a i) (get b i)) 1 0))
-
+(defn bit-comparison [i colla collb]
+  (if (> (get colla i) (get collb i)) 1 0))
 
 (defn power-levels->consumption
+  "Returns power consumption by decoding gamma and epsilon values"
   [{:keys [on-count off-count]}]
   (loop [i 0
          gamma (vec (repeat (count on-count) 0))
@@ -37,15 +46,14 @@
              (assoc gamma i (bit-comparison i on-count off-count))
              (assoc epsilon i (bit-comparison i off-count on-count))))))
 
-
-(defn power-level [readings]
-  (loop [remaining-readings readings
-         on (vec (repeat (count (first readings)) 0))
-         off (vec (repeat (count (first readings)) 0))]
+(defn power-consumption [diagnostics]
+  (loop [remaining-readings diagnostics
+         on (vec (repeat (count (first diagnostics)) 0))
+         off (vec (repeat (count (first diagnostics)) 0))]
     (if (empty? remaining-readings)
       (power-levels->consumption {:on-count (into [] on) :off-count (into [] off)})
       (recur (rest remaining-readings)
              (map + on (power-readout (first remaining-readings) on-setting))
              (map + off (power-readout (first remaining-readings) off-setting))))))
 
-(println "The power level is" (power-level (input "resources/input3")))
+(println "Part 1: the power level is" (power-consumption (input "resources/input3")))
